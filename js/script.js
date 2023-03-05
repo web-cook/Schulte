1a50a8d9fdbf8dc6508da8b985b757b90bee5036
Vladimir <web-cook@inbox.ru>
2023-03-05 18:12:57 +0300

    save results add

--- js/script.js
+++ js/script.js
@@ -1,43 +1,118 @@
-let cells = document.querySelectorAll('.field__cell');//список ячеек
-let field = document.querySelector('.field');//поле с числами
-let numbers = [];//массив для чисел
-let num;//переменная для хранения случайного числа из массива
-let seconds = document.querySelector('.timer__seconds');
-let milliseconds = document.querySelector('.timer__milliseconds');
-let start = document.querySelector('#start-button');
-let wrapper = document.querySelector('.button__wrapper');
-let results = document.querySelector('#results-button');
-let resultsWrapper = document.querySelector('.results');
-let resultsTitle = document.querySelector('.results__title');
-let resultsClose = document.querySelector('.results__close');
-let resultsList = document.querySelector('.results__list');
-let stop = document.querySelector('#stop-button');
-let resultsOpen = false;
-let timerStart = false;
+const cells = document.querySelectorAll('.field__cell'), //список ячеек
+  field = document.querySelector('.field'), //поле с числами
+  seconds = document.querySelector('.timer__seconds'),
+  milliseconds = document.querySelector('.timer__milliseconds'),
+  start = document.querySelector('#start-button'),
+  wrapper = document.querySelector('.button__wrapper'),
+  results = document.querySelector('#results-button'),
+  resultsWrapper = document.querySelector('.results'),
+  resultsTitle = document.querySelector('.results__title'),
+  resultsClose = document.querySelector('.results__close'),
+  resultsList = document.querySelector('.results__list'),
+  stop = document.querySelector('#stop-button');
 
-console.log(start);
+let numbers = [], //массив для чисел
+    num, //переменная для хранения случайного числа из массива
+    resultsOpen = false,
+    timerStart = false,
+    gameIsOn = false;
 
-function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
+//сохранение результатов в LocalStorage
+const keyName = 'schulte';
+
+let today = () => {
+  const date = new Date();
+  let d = date.getDate(),
+      m = date.getMonth() + 1,
+      y = date.getFullYear();
+
+  if (m < 10) m = `0${m}`;
+  if (d < 10) d = `0${d}`;
+
+  return `${d}.${m}.${y}`;
+}
+
+let games = {
+  gameId: {}
+};
+
+function storageSetData() {
+  saveResults();
+  localStorage.setItem(keyName, JSON.stringify(games));
+}
+
+function storageGetData() {
+  let key = keyName;
+  let previousData = localStorage.getItem(key);
+
+  let previousDataObj = JSON.parse(previousData);
+
+
+  games.gameId = previousDataObj.gameId;
+
+}
+
+let result = 0;
+
+function saveResults() {
+  games.gameId[`${keyName}-${Date.now()}`] = {
+    date: today(),
+    result: `${seconds.textContent}:${milliseconds.textContent}`
+  };
+}
+
+function updateStorage() {
+  result++;
+
+  if (keyName in localStorage) {
+    //достаем данные из localStorage, сохраняем в объект game
+    storageGetData();
+    //сохраняем новые результаты в объект game
+    storageSetData();
+  } else {
+    storageSetData();
   }
+}
+
+//запись результатов
+
+function writingResults() {
+  clearResultList();
+  let list = localStorage.getItem(keyName);
+  let listObj = JSON.parse(list);
+
+  for (const key in listObj) {
+    const element = listObj[key];
+    for (const key1 in element) {
+      const el = element[key1];
+      resultsList.innerHTML += `<li class="results__item"><span class="results__date">${el.date}</span><span class="results__time">${el.result}</span></li>`;
+    }
+  }
+}
+writingResults();
+
+//инициализация поля игры
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
 
 function fillArray() {
-    //заполнение массива
-    for(let i = 0; i < cells.length; i++) {
-        numbers[i] = i + 1;
-    }
-    //заполнение ячеек случайными числами из массива
-    for(let i = 0; i < cells.length; i++) {
-        num = numbers.splice(getRandomInt(numbers.length), 1);
-        cells[i].textContent = num;
-    }
+  //заполнение массива
+  for (let i = 0; i < cells.length; i++) {
+    numbers[i] = i + 1;
+  }
+  //заполнение ячеек случайными числами из массива
+  for (let i = 0; i < cells.length; i++) {
+    num = numbers.splice(getRandomInt(numbers.length), 1);
+    cells[i].textContent = num;
+  }
 }
 
 //очистка поля
 function clearArray() {
-    for(let el of cells) {
-        el.textContent = '';
-    }
+  for (let el of cells) {
+    el.textContent = '';
+  }
 }
 
 //старт игры
@@ -45,67 +120,80 @@ start.addEventListener('click', game)
 
 //игра
 function game() {
-    stop.style.display = 'block';
-    count = 1;
-    wrapper.style.display = 'none';
-    pastSec = 0;
-    pastMsec = 0;
-    fillArray();
-    timer();
-    start.removeEventListener('click', game);
+  gameIsOn = true;
+  stop.style.display = 'block';
+  count = 1;
+  wrapper.style.display = 'none';
+  clearTimer();
+  fillArray();
+  timer();
+  start.removeEventListener('click', game);
 }
 
 //таймер
-let count = 1;//внешний счетчик для чисел
+let count = 1; //внешний счетчик для чисел
 
-let pastSec = 0;//начальные значения таймера
+let pastSec = 0; //начальные значения таймера
 let pastMsec = 0;
+
+function clearTimer() {
+  pastSec = 0;
+  pastMsec = 0;
+  seconds.textContent = '00';
+  milliseconds.textContent = '00';
+}
 
 function timer() {
   let timerId = setTimeout(timer, 10);
 
-    if(pastSec < 10) {
-        seconds.textContent = `0${pastSec}`;
-    } else {
-        seconds.textContent = pastSec;
-    }
-    
-    if(pastMsec < 10) {
-        milliseconds.textContent = `0${pastMsec}`;
-    } else {
-        milliseconds.textContent = pastMsec;
-    }
-    
-    pastMsec++;
+  if (pastSec < 10) {
+    seconds.textContent = `0${pastSec}`;
+  } else {
+    seconds.textContent = pastSec;
+  }
 
-    if(pastMsec === 100) {
-        pastMsec = 0;
-        pastSec++;
+  if (pastMsec < 10) {
+    milliseconds.textContent = `0${pastMsec}`;
+  } else {
+    milliseconds.textContent = pastMsec;
+  }
+
+  pastMsec++;
+
+  if (pastMsec === 100) {
+    pastMsec = 0;
+    pastSec++;
+  }
+
+  stop.addEventListener('click', function () {
+    gameIsOn = false;
+    clearTimeout(timerId);
+    wrapper.style.display = 'flex';
+    clearTimer();
+    clearArray();
+    stop.style.display = 'none';
+    start.addEventListener('click', game);
+  })
+
+  field.addEventListener('click', function (e) {
+    if (e.target.textContent == count) {
+      e.target.textContent = '';
+      count++;
+
     }
+  })
 
-    stop.addEventListener('click', function () {
-        clearTimeout(timerId);
-        wrapper.style.display = 'flex';
-        seconds.textContent = '00';
-        milliseconds.textContent = '00';
-        clearArray();
-        stop.style.display = 'none';
-        start.addEventListener('click', game);
-    })
-
-    field.addEventListener('click', function (e) {
-        if(e.target.textContent == count) {
-            e.target.textContent = '';
-            count++;
-            console.log(count);
-        }
-    })
-
-    if(count === 26) {
-        resultsList.innerHTML += `<li>${seconds.textContent}:${milliseconds.textContent}</li>`;
-        clearTimeout(timerId);
-        start.addEventListener('click', game)
-    }
+  if (count === 26) {
+    updateStorage();
+    writingResults();
+    clearTimeout(timerId);
+    gameIsOn = false;
+    start.addEventListener('click', game);
+    setTimeout(() => {
+      showResults();
+      clearTimer();
+    }, 1000)
+  }
 }
 
 // список результатов
@@ -113,50 +201,51 @@ function timer() {
 let it = 0;
 
 results.addEventListener('click', () => {
-  if(!resultsOpen) {
+  if (!resultsOpen && !gameIsOn) {
     showResults();
   } else {
     hideResults();
   }
-  resultsOpen = !resultsOpen;
 })
 
 resultsClose.addEventListener('click', () => {
-    hideResults();
+  hideResults();
 })
 
 function showResults() {
-    it += 10;
-    resultsWrapper.style.width = `${it}px`;
-    if(resultsWrapper.clientWidth >= field.clientWidth) {
-        resultsWrapper.style.width = `${field.clientWidth}px`;
-        resultsTitle.style.visibility = 'visible';
-        resultsClose.style.display = 'block';
-        resultsList.style.visibility = 'visible';
-        console.log(field.clientWidth);
-        console.log(resultsWrapper.clientWidth);
-        return;
-    }
-    setTimeout(showResults, 5); 
+  resultsOpen = true;
+  it += 10;
+  resultsWrapper.style.width = `${it}px`;
+  if (resultsWrapper.clientWidth >= field.clientWidth) {
+    resultsWrapper.style.width = `${field.clientWidth}px`;
+    resultsTitle.style.visibility = 'visible';
+    resultsClose.style.display = 'block';
+    resultsList.style.visibility = 'visible';
+
+
+    return;
+  }
+  setTimeout(showResults, 5);
 }
 
 function hideResults() {
-    if(resultsTitle.style.visibility === 'visible') {
-        resultsTitle.style.visibility = 'hidden';
-        resultsClose.style.display = 'none';
-        resultsList.style.visibility = 'hidden';
-    }
-    it -= 10;
-    resultsWrapper.style.width = `${it}px`;
-    if(resultsWrapper.clientWidth <= 0) {
-        resultsWrapper.style.width = '0px';
-        console.log(resultsWrapper.clientWidth);
-        return;
-    }
-    setTimeout(hideResults, 5); 
+  resultsOpen = false;
+
+  if (resultsTitle.style.visibility === 'visible') {
+    resultsTitle.style.visibility = 'hidden';
+    resultsClose.style.display = 'none';
+    resultsList.style.visibility = 'hidden';
+  }
+  it -= 10;
+  resultsWrapper.style.width = `${it}px`;
+  if (resultsWrapper.clientWidth <= 0) {
+    resultsWrapper.style.width = '0px';
+
+    return;
+  }
+  setTimeout(hideResults, 5);
 }
 
-// console.log(resultsWrapper.style.width);
-
-// console.log(typeof resultsWrapper.style.width);
-// console.log(typeof +resultsWrapper.style.width);
+function clearResultList() {
+  resultsList.innerHTML = '';
+}
